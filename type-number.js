9dd703289e22783c307fa3dda086b95fe78be31e
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-03-03 15:25:18 +0000

    .

--- type-number.js
+++ type-number.js
@@ -2,6 +2,11 @@
 const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
-const operacaoMatematica = primeiroNumero + segundoNumero;
+const operacaoMatematica = primeiroNumero / segundoNumero;
 
-console.log(operacaoMatematica)
+console.log(operacaoMatematica);
+
+// ponto flutunte 
+
+const numeroPontoFlutuante = 3.3;
+const 
