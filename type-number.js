9dbeaa5ed31da834882682e956df2e0edc59f897
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-03-12 14:43:18 +0000

    não é número

--- type-number.js
+++ type-number.js
@@ -13,4 +13,9 @@ const pontoFlutuanteSemZero = .5;
 
 const novaOperacao = primeiroNumero / numeroPontoFlutuante;
 
-console.log (novaOperacao);
+console.log (novaOperacao); 
+
+//NaN -> Not a Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
